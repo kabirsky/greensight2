55ad5b5d3f2223b49a256b92b32716384572e2e4
kabirsky <kabirsky@yandex.ru>
2020-10-13 07:52:36 +0300

    Updates

---
+++ precache-manifest.77e81c5000c06e80a057a6e0ebea010a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b8a5c794aa4c034b5ff4a58bcce507",
+    "url": "/greensight2/index.html"
+  },
+  {
+    "revision": "a87604c796c7f0054802",
+    "url": "/greensight2/static/css/main.28b91e29.chunk.css"
+  },
+  {
+    "revision": "e2289cbaeceb9ea79f7c",
+    "url": "/greensight2/static/js/2.e5f1a6ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greensight2/static/js/2.e5f1a6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87604c796c7f0054802",
+    "url": "/greensight2/static/js/main.b2ab5b15.chunk.js"
+  },
+  {
+    "revision": "bbdb90b1a8b9c3f50b57",
+    "url": "/greensight2/static/js/runtime-main.95f630c9.js"
+  },
+  {
+    "revision": "09ce47bec4f96ac8132a2e676697b757",
+    "url": "/greensight2/static/media/Cross.09ce47be.svg"
+  }
+]);
